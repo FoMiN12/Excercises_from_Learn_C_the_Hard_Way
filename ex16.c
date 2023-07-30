23079ca610d8d7fbac8dcb1ce0243ea73a4c4927
Kirill Fomin <fomin102011@yandex.ru>
2023-07-30 23:37:38 +0300

    Added comments to mostly all lines in ex16.c

--- ex16.c
+++ ex16.c
@@ -1,45 +1,72 @@
+// including library for work with standart input output functional
 #include <stdio.h>
+// verify program assertion library
 #include <assert.h>
+// standart library definition
 #include <stdlib.h>
+// string operations
 #include <string.h>
 
+// define a structure
 struct Person {
+	// define a char pointer for name string
 	char *name;
+	// define age variable
 	int age;
+	// define height variable
 	int height;
+	// define weight variable
 	int weight;
 };
 
+// define a function that create and returns pointer on instance of struct Person
 struct Person *Person_create(char *name, int age, int height,
 		int weight)
-{
+{	
+	// allocation of memory for insctance of strauct Person and initializing pointer on addres of this allocation
 	struct Person *who = malloc(sizeof(struct Person));
+	// check for memory allocation
 	assert(who != NULL);
-
+	
+	// initializing name field with name string via strdup
 	who->name = strdup(name);
+	// initializing age field with age
 	who->age = age;
+	// initializing height field with height
 	who->height = height;
+	// initializing weight field with weight
 	who->weight = weight;
-
+	
+	// return pointer on created instance of struct Person
 	return who;
 }
 
+// define function for deleting struct Person from memory
 void Person_destroy(struct Person *who)
 {
+	// check for valide pointer argument
 	assert(who != NULL);
 	
+	// clearing memory of name field
 	free(who->name);
+	// clearing memory of whole struct
 	free(who);
 }
 
+// define function for printing information about struct Person instance
 void Person_print(struct Person *who)
 {
+	// print name field
 	printf("name: %s\n", who->name);
+	// print age field
 	printf("\tAge: %d\n", who->age);
+	// print height field
 	printf("\tHeight: %d\n", who->height);
+	// print weight field
 	printf("\tWeight: %d\n", who->weight);
 }
 
+// define main function
 int main(int argc, char *argv[])
 {
 	// make two people structures
